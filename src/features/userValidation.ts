4abfcb038f88db99fe5fce0779f03a8168ec8dc0
edik171989@gmail.com <edik171989@gmail.com>
2025-05-13 14:47:36 +0300

    feat: add validation schema for ads submission

--- src/features/userValidation.ts
+++ src/features/userValidation.ts
@@ -62,3 +62,31 @@ export const editProfileSchema = baseUserSchema.extend({
     ),
   location: z.string().optional(),
 })
+
+export const addAdsSchema = z.object({
+  title: z
+    .string()
+    .nonempty('Заголовок є обовʼязковим')
+    .max(100, 'Заголовок має містити не більше 100 символів'),
+  description: z
+    .string()
+    .nonempty('Опис є обовʼязковим')
+    .max(1000, 'Опис має містити не більше 1000 символів'),
+  category: z.string(),
+  images: z
+    .array(
+      z
+        .union([z.string().url(), z.instanceof(File)])
+        .refine(
+          (file) => !file || typeof file === 'string' || file.size < 5_000_000,
+          'Файл має бути менше 5MB',
+        )
+        .refine(
+          (file) =>
+            !file || typeof file === 'string' || ['image/jpeg', 'image/png'].includes(file.type),
+          'Тільки JPEG/PNG',
+        ),
+    )
+    .max(5, 'Максимум 5 зображень'),
+  price: z.number().nonnegative('Ціна не може бути відʼємною').min(1, 'Ціна має бути більшою за 0'),
+})
