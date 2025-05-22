003fc4e672b3ae91505b6c78b8c0108f3588ccb3
Eduard <edik171989@gmail.com>
2025-05-22 20:52:52 +0300

    feat: integrate toast notifications for ad submission success and error handling

--- src/api/adsCreate.ts
+++ src/api/adsCreate.ts
@@ -2,8 +2,6 @@ import { useMutation } from '@tanstack/react-query'
 import useBackendRequest from '../hooks/useBackendRequest'
 import { API_ENDPOINTS } from '../paths'
 import { TAdsCreate } from '../types/adsTypes'
-import toast from 'react-hot-toast'
-import { ApiError } from '../types/types'
 
 const adsCreate = () => {
   const fetchLogin = useBackendRequest()
@@ -11,18 +9,6 @@ const adsCreate = () => {
   return useMutation({
     mutationFn: async (data: TAdsCreate) =>
       await fetchLogin({ path: API_ENDPOINTS.ADS.create, method: 'POST', data }),
-    onSuccess: () => {
-      toast.success('Оголошення відправлено на модерацію', {
-        id: 'add_ads',
-        duration: 2000,
-      })
-    },
-    onError: (error: ApiError) => {
-      toast.error(`Невідома помилка ${error}`, {
-        id: 'add_ads',
-        duration: 2000,
-      })
-    },
   })
 }
 
--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { AdsImageUploader } from '../components'
 import { z } from 'zod'
 import adsCreate from '../api/adsCreate'
+import toast, { Toaster } from 'react-hot-toast'
+import { ApiError } from '../types/types'
 
 export type TFormData = z.infer<typeof addAdsSchema>
 
@@ -30,6 +32,7 @@ const AddProduct = () => {
     handleSubmit,
     watch,
     setValue,
+    reset,
     formState: { errors },
   } = useForm<TFormData>({
     defaultValues,
@@ -47,54 +50,60 @@ const AddProduct = () => {
       // category: data.category,
     }
 
-    createAds(newAds)
+    try {
+      await createAds(newAds)
+      toast.success('Оголошення відправлено на модерацію', { id: 'add_ads', duration: 2000 })
+      reset()
+    } catch (error) {
+      toast.error(`Невідома помилка ${error}`, { id: 'add_ads', duration: 2000 })
+    }
   }
 
   const watchCategory = watch('category')
 
   return (
     <div className="mx-auto max-w-7xl px-4 pt-24">
-      <h1 className="text-px32 mb-24 font-medium">Створити оголошення</h1>
+      <h1 className="mb-24 text-px32 font-medium">Створити оголошення</h1>
       <form onSubmit={handleSubmit(handleSubmitForm)}>
         {/* Секція для заголовка оголошення */}
         <section className="mb-10">
-          <h2 className="text-px24 mb-8 font-medium">Заголовок оголошення</h2>
-          <p className="font-regular text-px16 mb-5">
+          <h2 className="mb-8 text-px24 font-medium">Заголовок оголошення</h2>
+          <p className="font-regular mb-5 text-px16">
             Введіть коротку назву товару (до 100 символів)
           </p>
           <input
             {...register('title')}
             type="text"
             placeholder="Приклад: iPhone 12, 128 Gb"
-            className={`w-full rounded-full bg-[#D9D9D9] px-5 py-2 outline-none placeholder:text-[#4D4D4D] ${errors.title ? 'outline-1 outline-error' : ''}`}
+            className={`w-full rounded-full bg-[#D9D9D9] px-5 py-2 outline-none placeholder:text-[#4D4D4D] ${errors.title ? 'outline-error outline-1' : ''}`}
           />
           {errors.title && <p className="error-text">{errors.title.message}</p>}
         </section>
 
         {/* Секція для опису товару */}
         <section className="mb-10">
-          <h2 className="text-px24 mb-8 font-medium">Опис</h2>
-          <p className="font-regular text-px16 mb-5">
+          <h2 className="mb-8 text-px24 font-medium">Опис</h2>
+          <p className="font-regular mb-5 text-px16">
             Детально опишіть товар, стан, особливості тощо
           </p>
           <textarea
             {...register('description')}
             rows={3}
             placeholder="Приклад: Б/у, ідеальний стан, користувався обережно. Повна комплектація."
-            className={`w-full rounded-3xl bg-[#D9D9D9] px-5 py-2 outline-none placeholder:text-[#4D4D4D] ${errors.description ? 'outline-1 outline-error' : ''}`}
+            className={`w-full rounded-3xl bg-[#D9D9D9] px-5 py-2 outline-none placeholder:text-[#4D4D4D] ${errors.description ? 'outline-error outline-1' : ''}`}
           ></textarea>
           {errors.description && <p className="error-text">{errors.description.message}</p>}
         </section>
 
         {/* Секція для вибору категорії */}
         <section className="mb-10">
-          <h2 className="text-px24 mb-8 font-medium">Категорія</h2>
+          <h2 className="mb-8 text-px24 font-medium">Категорія</h2>
 
           <div className="flex flex-wrap gap-5">
             {Object.entries(categories).map(([key, value]) => {
               return (
                 <label
-                  className={`text-regular transition-[background-color, color] text-px16 cursor-pointer rounded-full border border-[#2D336B] px-4 py-2 duration-300 ${
+                  className={`text-regular transition-[background-color, color] cursor-pointer rounded-full border border-[#2D336B] px-4 py-2 text-px16 duration-300 ${
                     watchCategory === key
                       ? 'bg-[#2D336B] text-white'
                       : 'text-[#2D336B] hover:bg-[#2D336B] hover:text-white'
@@ -117,8 +126,8 @@ const AddProduct = () => {
 
         {/* Секція для завантаження зображень */}
         <section className="mb-10">
-          <h2 className="text-px24 mb-8 font-medium">Додайте зображення</h2>
-          <p className="font-regular text-px16 mb-5">
+          <h2 className="mb-8 text-px24 font-medium">Додайте зображення</h2>
+          <p className="font-regular mb-5 text-px16">
             Перетягніть файли або натисніть для завантаження
           </p>
 
@@ -127,14 +136,14 @@ const AddProduct = () => {
 
         {/* Секція для введення ціни */}
         <section className="mb-10">
-          <h2 className="text-px24 mb-8 font-medium">Ціна</h2>
-          <p className="font-regular text-px16 mb-5">Введіть ціну без ком/крапок</p>
+          <h2 className="mb-8 text-px24 font-medium">Ціна</h2>
+          <p className="font-regular mb-5 text-px16">Введіть ціну без ком/крапок</p>
           <input
             type="number"
             {...register('price')}
             placeholder="Наприклад: 12000"
             className={`w-full rounded-3xl bg-[#D9D9D9] px-5 py-2 outline-none placeholder:text-[#4D4D4D] ${
-              errors.price ? 'outline-1 outline-error' : ''
+              errors.price ? 'outline-error outline-1' : ''
             }`}
           />
           {errors.price && <p className="error-text">{errors.price.message}</p>}
@@ -144,6 +153,14 @@ const AddProduct = () => {
           Опублікувати
         </button>
       </form>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          id: 'profile-editor-toasts',
+          className:
+            '!bg-[#FFFFFF] !text-[1.5rem]  text-[#000000] rounded-[100px] flex flex-row-reverse !max-w-none !w-fit !whitespace-nowrap px-[1.25rem] py-[0.625rem]',
+        }}
+      />
     </div>
   )
 }
