344699a95a2250b0025757b1ee294c8e58b97f3e
Eduard <edik171989@gmail.com>
2025-05-22 21:02:12 +0300

    fix: correct input class order and change button to link for consistency

--- src/hoc/PageWrapper/PageWrapper.tsx
+++ src/hoc/PageWrapper/PageWrapper.tsx
@@ -21,7 +21,7 @@ const PageWrapper = () => {
             <input
               type="text"
               placeholder="Пошук товарів"
-              className="text-px16 placeholder:text-px16 h-[40px] w-full rounded-[20px] border-0.5 border-[#FFFFFF] bg-[#2D336B] pl-10 pr-4 font-normal placeholder:font-normal"
+              className="h-[40px] w-full rounded-[20px] border-0.5 border-[#FFFFFF] bg-[#2D336B] pl-10 pr-4 text-px16 font-normal placeholder:text-px16 placeholder:font-normal"
             />
             <img
               src={searchIcon}
@@ -31,7 +31,7 @@ const PageWrapper = () => {
           </div>
           <Link to={PATHS.PRODUCTS.add} className="button bg-white text-[#000000]">
             Створити оголошення
-          </button>
+          </Link>
           <PrivateRoute>
             <Link to={PATHS.PROFILE.profile}>
               <img
