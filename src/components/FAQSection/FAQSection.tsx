107ffb2ecc4a5e70f8fe133f912f523f13b87bac
Eduard <edik171989@gmail.com>
2025-07-02 10:53:48 +0300

    refactor: remove static FAQ items and fetch from API for dynamic content

--- src/components/FAQSection/FAQSection.tsx
+++ src/components/FAQSection/FAQSection.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import FAQItem from '../FAQItem/FAQItem'
-import { FAQItems } from '../../assets/FAQ_items'
+import useFetchFAQItems from '../../api/useFetchFAQItems'
 
 interface FAQSectionProps {}
 
 const FAQSection: React.FC<FAQSectionProps> = ({}) => {
+  const { data: FAQItems } = useFetchFAQItems()
+
   return (
     <section className="mx-auto flex max-w-container flex-col items-center justify-center py-[7.5rem]">
       <h2 className="self-center text-center text-h2">FAQ</h2>
       <div className="w-[70%]">
-        {FAQItems.map((item) => (
-          <FAQItem {...item} key={item.question} />
-        ))}
+        {FAQItems && FAQItems.map((item) => <FAQItem {...item} key={item.question} />)}
       </div>
     </section>
   )
